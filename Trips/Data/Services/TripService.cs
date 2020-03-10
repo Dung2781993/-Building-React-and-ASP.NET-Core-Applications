ad296a8f9b46822ee823888815a651cb9fc7c1ef
Dung2781993@gmail.com <Dung2781993@gmail.com>
2020-03-11 01:42:19 +1100

    Setup mock services and data

---
+++ Trips/Data/Services/TripService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trips.Data.Services
+{
+    public class TripService : ITripService
+    {
+        public void AddTrip(Trip trip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTrip(int tripId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Trip> GetAllTrips()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Trip GetTripById(int tripId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTrip(int tripId, Trip trip)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Trips/Startup.cs
+++ Trips/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Trips.Data;
+using Trips.Data.Services;
 
 namespace Trips
 {
@@ -28,6 +30,8 @@ namespace Trips
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddTransient<ITripService, TripService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
