97ed896dd0ac2403dea6879b89059000327040ad
Dung2781993@gmail.com <Dung2781993@gmail.com>
2020-03-14 01:40:14 +1100

    Update data from

--- Trips/ClientApp/src/App.js
+++ Trips/ClientApp/src/App.js
@@ -4,6 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Trips } from './components/Trip/Trips';
 import { Create } from './components/Trip/Create';
+import { Update } from './components/Trip/Update';
 import './custom.css'
 
 
@@ -16,6 +17,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/create' component={Create} />
         <Route path='/trips' component={Trips} />
+        <Route path='/update/:id' component={Update} />
       </Layout>
     );
   }
--- Trips/ClientApp/src/components/Trip/Trips.jsx
+++ Trips/ClientApp/src/components/Trip/Trips.jsx
@@ -1,28 +1,43 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-
-
 export class Trips extends Component
 {
     constructor(props){
         super(props);
 
+        this.onTripUpdate = this.onTripUpdate.bind(this);
+        this.onTripDelete = this.onTripDelete.bind(this);
+
         this.state = {
             trips: [],
-            loading: true
+            loading: true,
+            failed: false,
+            error: ''
         }
     }
+
     componentDidMount(){
         this.populateTripsData();
+    }
+
+    onTripUpdate(id){
+        const {history} = this.props;
+        history.push('/update/'+id);
+    }
+
+    onTripDelete(id){
+        const {history} = this.props;
+        history.push('/delete/'+id);
     }
 
     populateTripsData(){
         axios.get("api/Trips/GetTrips").then(result => {
             const response = result.data;
-            this.setState({trips: response, loading: false});
-        })
+            this.setState({trips: response, loading: false, failed: false, error:""});
+        }).catch(error => {
+            this.setState({trips: [], loading: false, failed: true, error:"Trips could not be loaded"});
+        });
     }
-
 
     renderAllTripsTable(trips){
         return (
@@ -37,20 +52,30 @@ export class Trips extends Component
                     </tr>
                 </thead>
                 <tbody>
-                {
+                    {
                         trips.map(trip => (
                         <tr key={trip.id}>
                             <td>{trip.name}</td>
                             <td>{trip.description}</td>
-                            <td>{new Date(trip.dateStarted).toLocaleDateString()}</td>
-                            <td>{trip.dateCompleted ? new Date(trip.dateCompleted).toLocaleDateString() :  '-' }</td>
-                            <td> - </td>
+                            <td>{new Date(trip.dateStarted).toISOString().slice(0,10)}</td>
+                            <td>{trip.dateCompleted ? new Date(trip.dateCompleted).toISOString().slice(0,10) :  '-' }</td>
+                            <td>
+                                <div className="form-group">
+                                    <button onClick={() => this.onTripUpdate(trip.id)} className="btn btn-success">
+                                        Update
+                                    </button>
+                                    <button onClick={() => this.onTripDelete(trip.id)} className="btn btn-danger">
+                                        Delete
+                                    </button>
+                                </div>
+                            </td>
                         </tr>
                         ))
                     }
+                    
                 </tbody>
             </table>
-        )
+        );
     }
 
     render(){
@@ -59,16 +84,20 @@ export class Trips extends Component
             <p>
                 <em>Loading...</em>
             </p>
+        ) : ( this.state.failed ? (
+            <div className="text-danger">
+                <em>{this.state.error}</em>
+            </div>
         ) : (
-            this.renderAllTripsTable(this.state.trips)
+            this.renderAllTripsTable(this.state.trips))
         )
 
-        return(
+        return (
             <div>
                 <h1>All trips</h1>
-                <p>You can see all trips</p>
+                <p>Here you can see all trips</p>
                 {content}
             </div>
-        );                                                                   
+        );
     }
 }
